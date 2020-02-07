11426f7e1fef7ff43ec8d4f55e28203a515d809d
Shivam Kochar <shivam.kochar7@gmail.com>
2020-02-06 20:09:52 -0500

    Completed Rectangle class

---
+++ Assignment01/Rectangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01
+{
+    public class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public Rectangle()
+        {
+            this.length = 1;
+            this.width = 1;
+        }
+
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public int GetLength()
+        {
+            return this.length;
+        }
+
+        public int SetLength(int length)
+        {
+            this.length = length;
+            return length;
+        }
+
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return width;
+        }
+
+        public int GetPerimeter()
+        {
+            return 2 * (GetLength() + GetWidth());
+        }
+
+        public int GetArea()
+        {
+            return GetLength() * GetWidth();
+        }
+    }
+}
